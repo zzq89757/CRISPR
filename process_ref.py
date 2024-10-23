7bfaa26225061f5f4d88c70bd48426b0ebf0623c
zzq89757 <1604653835@qq.com>
2024-10-23 09:14:56 +0000

    position repaired

--- process_ref.py
+++ process_ref.py
@@ -2,7 +2,7 @@ from pysam import FastaFile
 from multiprocessing import Pool, RLock
 
 
-def async_in_iterable_structure(fun, fun_args, iterable_structure, cpus):
+def async_in_iterable_structure(fun, iterable_structure, cpus):
     def init(l):
         global lock
         lock = l
@@ -11,7 +11,7 @@ def async_in_iterable_structure(fun, fun_args, iterable_structure, cpus):
     p = Pool(int(cpus), initializer=init, initargs=(lock,))
     # apply async in iterable structure
     for i in iterable_structure:
-        p.apply_async(func=fun, args=[i, fun_args])
+        p.apply_async(func=fun, args=[i])
     p.close()
     p.join()
 
@@ -20,14 +20,16 @@ def reverse_complement(seq: str) -> str:
     trantab = str.maketrans("ACGTNacgtnRYMKrymkVBHDvbhd", "TGCANtgcanYRKMyrkmBVDHbvdh")
     return seq.translate(trantab)[::-1]
 
-def obtain_ref_info(ref: str) -> list:
+def obtain_chr_li(ref: str) -> list:
     ref_handle = FastaFile(ref)
-    return ref_handle.references, ref_handle
+    chr_li = ref_handle.references
+    ref_handle.close()
+    return chr_li
 
 
-def generate_sgRNA_table(chr_name: str, ref):
+def generate_sgRNA_table(chr_name: str):
 
-    # ref = FastaFile("/mnt/ntc_data/wayne/DataBase/Homo/hg38.fa")
+    ref = FastaFile("/mnt/ntc_data/wayne/DataBase/Homo/hg38.fa")
     output = open(f"./split_out/Homo_{chr_name}.tsv", "w")
     print("gRNA Sequence\tPAM\tchr\tchr_strand\tgRNA_start(in chr)\tgRNA_end(in chr)\tgRNA_cut(in chr)", file=output)
     # print(dir(ref))
@@ -55,12 +57,12 @@ def generate_sgRNA_table(chr_name: str, ref):
             and chr_seq[i].upper() == "G"
             and (chr_seq[i - 1].upper() == "A" or chr_seq[i - 1].upper() == "G")
         ):
-            sgRNA = chr_seq[i - 22 : i - 1].upper()
+            sgRNA = chr_seq[i - 22 : i - 2].upper()
             pam_seq = chr_seq[i - 2 :i + 1].upper()
             grna_start = i - 21
-            grna_end = i - 1
-            grna_cut = i - 4
-            detail = f"{sgRNA}\t{pam_seq}\t{chr_name}+\t{grna_start}\t{grna_end}\t{grna_cut}"
+            grna_end = i - 2
+            grna_cut = i - 5
+            detail = f"{sgRNA}\t{pam_seq}\t{chr_name}\t+\t{grna_start}\t{grna_end}\t{grna_cut}"
             print(detail,file=output)
             # print(no, end="\t", file=output)
             # print(sgRNA, end="\n", file=output)
@@ -84,7 +86,7 @@ def generate_sgRNA_table(chr_name: str, ref):
             grna_start = i + 23
             grna_end = i + 4
             grna_cut = i + 20
-            detail = f"{sgRNA}\t{pam_seq}\t{chr_name}-\t{grna_start}\t{grna_end}\t{grna_cut}"
+            detail = f"{sgRNA}\t{pam_seq}\t{chr_name}\t-\t{grna_start}\t{grna_end}\t{grna_cut}"
             print(detail,file=output)
             # print(no, end="\t", file=output)
             # print(chr_name, end="\t", file=output)
@@ -99,5 +101,10 @@ def generate_sgRNA_table(chr_name: str, ref):
             # exit()
 
 def main() -> None:
-    chr_li, ref_handle = obtain_ref_info("/mnt/ntc_data/wayne/DataBase/Homo/hg38.fa")
-    async_in_iterable_structure(generate_sgRNA_table, ref_handle[:2], chr_li, 24)
+    chr_li = obtain_chr_li("/mnt/ntc_data/wayne/DataBase/Homo/hg38.fa")
+    async_in_iterable_structure(generate_sgRNA_table, chr_li[-1:], 10)
+    # generate_sgRNA_table('chr1',ref_handle)
+    
+
+if __name__ == "__main__":
+    main()
