b990fa62e8d451fe02b5218f209353031b62ed15
zzq89757 <1604653835@qq.com>
2024-10-24 04:25:04 +0000

    sort and merge shell finished

--- split_out/sort_by_xsv.sh
+++ split_out/sort_by_xsv.sh
@@ -6,7 +6,7 @@
 # csvsort -t -c 5,6 ./Homo_chrY_KI270740v1_random.tsv | csvformat -T
 
 # LC_ALL=C sort -t$'\t' -k5,5n -k4 ./Homo_chrY_KI270740v1_random.tsv |awk '{if(NR==1) print "gRNA-ID", $0;else print NR-1, $0}' 
-LC_ALL=C sort -t$'\t' -k5,5n -k4 ./Homo_chrY_KI270740v1_random.tsv |awk 'NR==1 { print "gRNA-ID", $0} NR>1{ print NR-1, $0}' 
+LC_ALL=C sort -t$'\t' -k5,5n -k4 ./Homo_chrY_KI270740v1_random.tsv |gawk 'NR==1 { print "gRNA-ID", $0} NR>1{ print NR-1, $0}' 
 # awk FNR 多文件行号 sort完后多个文件一起加编号（去表头）
 # LC_ALL=C sort -t$'\t'  -k5,5n -k4  Homo_chr1.tsv
 # LC_ALL=C sort
