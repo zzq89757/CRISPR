9736f50fdf3f256f8cf138e4c26deeed57cd30ae
zzq89757 <1604653835@qq.com>
2024-10-24 08:28:15 +0000

    final edition finished

---
+++ split_out/all_chr_sort.sh
@@ -0,0 +1,3 @@
+LC_ALL=C sort -t$'\t' -k5,5n -k4 Homo_chr10.tsv > sorted/Homo_chr10.tsv&
+LC_ALL=C sort -t$'\t' -k5,5n -k4 Homo_chr1.tsv > sorted/Homo_chr1.tsv&
+LC_ALL=C sort -t$'\t' -k5,5n -k4 Homo_chrY_KI270740v1_random.tsv > sorted/Homo_chrY_KI270740v1_random.tsv&
--- split_out/sort_by_xsv.sh
+++ split_out/sort_by_xsv.sh
@@ -6,7 +6,21 @@
 # csvsort -t -c 5,6 ./Homo_chrY_KI270740v1_random.tsv | csvformat -T
 
 # LC_ALL=C sort -t$'\t' -k5,5n -k4 ./Homo_chrY_KI270740v1_random.tsv |awk '{if(NR==1) print "gRNA-ID", $0;else print NR-1, $0}' 
-LC_ALL=C sort -t$'\t' -k5,5n -k4 ./Homo_chrY_KI270740v1_random.tsv |gawk 'NR==1 { print "gRNA-ID", $0} NR>1{ print NR-1, $0}' 
-# awk FNR 多文件行号 sort完后多个文件一起加编号（NR去表头 FNR留表头）
 # LC_ALL=C sort -t$'\t'  -k5,5n -k4  Homo_chr1.tsv
 # LC_ALL=C sort
+
+# scan genome and generate database of single chr
+python /mnt/ntc_data/wayne/Repositories/CRISPR/process_ref.py
+
+# final valid sort and merge command
+
+LC_ALL=C sort -t$'\t' -k5,5n -k4 ./Homo_chrY_KI270740v1_random.tsv |gawk 'NR==1 { print "gRNA-ID", $0} NR>1{ print NR-1, $0}' 
+# awk FNR 多文件行号 sort完后多个文件一起加编号（NR去表头 FNR留表头）
+# ls Homo*tsv |xargs -I {} echo "LC_ALL=C sort -t \$\'\\\t\' -k5,5n -k4 {}"|xargs -I {} -P 24 bash {}
+# ls Homo*tsv |xargs -I {} echo "LC_ALL=C sort -t$\'\\\t\' -k5,5n -k4 {}" |xargs -I {} -P 24 bash {}
+# ls Homo*tsv |xargs -I {} echo "LC_ALL=C sort -t$\'\t\' -k5,5n -k4 {} > sorted/{}" > all_chr_sort.sh
+# cat all_chr_sort.sh|xargs -I {} -P 24 bash -c {}
+mkdir sorted
+ls Homo*tsv |xargs -I {} echo "LC_ALL=C sort -t$'\t' -k5,5n -k4 {} > sorted/{}&" > all_chr_sort.sh
+bash all_chr_sort.sh
+
