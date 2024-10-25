9d1f9b77c6fb28b62b40ca465f4516527d0d3862
zzq89757 <1604653835@qq.com>
2024-10-25 02:06:55 +0000

    final edition finished

--- split_out/sort_by_xsv.sh
+++ split_out/sort_by_xsv.sh
@@ -14,7 +14,8 @@ python /mnt/ntc_data/wayne/Repositories/CRISPR/process_ref.py
 
 # final valid sort and merge command
 
-LC_ALL=C sort -t$'\t' -k5,5n -k4 ./Homo_chrY_KI270740v1_random.tsv |gawk 'NR==1 { print "gRNA-ID", $0} NR>1{ print NR-1, $0}' 
+# LC_ALL=C sort -t$'\t' -k5,5n -k4 ./Homo_chrY_KI270740v1_random.tsv |gawk 'NR==1 { print "gRNA-ID", $0} NR>1{ print NR-1, $0}' 
+
 # awk FNR 多文件行号 sort完后多个文件一起加编号（NR去表头 FNR留表头）
 # ls Homo*tsv |xargs -I {} echo "LC_ALL=C sort -t \$\'\\\t\' -k5,5n -k4 {}"|xargs -I {} -P 24 bash {}
 # ls Homo*tsv |xargs -I {} echo "LC_ALL=C sort -t$\'\\\t\' -k5,5n -k4 {}" |xargs -I {} -P 24 bash {}
