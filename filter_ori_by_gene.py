58dbd6fb34cdc2ebebc65af32e048c19b20b6a96
zzq89757 <1604653835@qq.com>
2024-12-09 09:44:23 +0000

    nc2chr

--- filter_ori_by_gene.py
+++ filter_ori_by_gene.py
@@ -12,13 +12,13 @@ def main() -> None:
     type_li = ["string", "int32", "int32", "category", "int32", "category"]
     type_dict = dict(enumerate(type_li))
     # 读取gRNA db 文件 
-    gdb_df = pd.read_csv()
+    gdb_df = pd.read_csv(f"/mnt/ntc_data/wayne/Repositories/CRISPR/split_out/sorted/no_head/{chr}")
     # 读取基因位置信息文件
     gene_pos_df = pd.read_csv(f"/mnt/ntc_data/wayne/Repositories/CRISPR/split_gtf/{nc_no}/Gene_list.tsv", sep="\t", header=None, low_memory=False, dtype=type_dict)
     # gdb 注释
     annotated_df = annotation_gdb(gdb_df, gene_pos_df)
     # 保存为tsv文件
-    annotated_df.to_csv()
+    annotated_df.to_csv(f"/mnt/ntc_data/wayne/Repositories/CRISPR/split_out/sorted/no_head/{chr}")
     
 
 if __name__ == "__main__":
