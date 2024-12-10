109670da6c9835517956a92bd2e7f0905b802f3f
zzq89757 <1604653835@qq.com>
2024-12-10 08:02:21 +0000

    type declear

--- filter_ori_by_gene_vector.py
+++ filter_ori_by_gene_vector.py
@@ -25,38 +25,50 @@ def annotation_gdb(gdb: pd.DataFrame, gene_pos_df: pd.DataFrame) -> pd.DataFrame
                 break
 
         # 如果 gRNA 位于当前基因范围内，记录信息
-        if gene_start[current_gene_pos_idx] <= g_pos <= gene_end[current_gene_pos_idx]:
+        if g_pos <= gene_end[current_gene_pos_idx]:
             # print(f"{g_pos} in {gene_start[current_gene_pos_idx]}-{gene_end[current_gene_pos_idx]}", flush=True, end="\r")
-            print(f"{g_pos} in {gene_start[current_gene_pos_idx]}-{gene_end[current_gene_pos_idx]}")
-            
-    
+            # print(f"{g_pos} in {gene_start[current_gene_pos_idx]}-{gene_end[current_gene_pos_idx]}")
+            ...
+        else:
+            print("nope")
+
     return pd.DataFrame([])
 
 
 def main() -> None:
     t = time.time()
     nc_no = "NC_000024.10"
-    chr = 'chrY'
-    
+    chr = "chrY"
+
     nc_table = "/mnt/ntc_data/wayne/Repositories/CRISPR/nc2chr.tsv"
-    df = pd.read_csv(nc_table,sep="\t",header=None)
+    df = pd.read_csv(nc_table, sep="\t", header=None)
     chr2nc_dict = df.set_index(1)[0].to_dict()
     type_li = ["string", "int32", "int32", "category", "string", "category"]
     type_dict = dict(enumerate(type_li))
     # 读取基因位置信息文件
-    gene_pos_df = pd.read_csv(f"/mnt/ntc_data/wayne/Repositories/CRISPR/split_gtf/{chr2nc_dict[chr]}/Gene_list_cut_insertion.tsv", sep="\t", header=None, low_memory=False, dtype=type_dict)
-    # 读取gRNA db 文件 
-    type_li = ['string', 'string', 'category', 'category', 'int32', 'int32', 'int32']
-    type_dict= dict(enumerate(type_li))
-    gdb_df = pd.read_csv(f"/mnt/ntc_data/wayne/Repositories/CRISPR/split_out/sorted/no_head/spCas9_Homo_{chr}.tsv", header=None, sep="\t")
+    gene_pos_df = pd.read_csv(
+        f"/mnt/ntc_data/wayne/Repositories/CRISPR/split_gtf/{chr2nc_dict[chr]}/Gene_list_cut_insertion.tsv",
+        sep="\t",
+        header=None,
+        low_memory=False,
+        dtype=type_dict,
+    )
+    # 读取gRNA db 文件
+    type_li = ["string", "string", "category", "category", "int32", "int32", "int32"]
+    type_dict = dict(enumerate(type_li))
+    gdb_df = pd.read_csv(
+        f"/mnt/ntc_data/wayne/Repositories/CRISPR/split_out/sorted/no_head/spCas9_Homo_{chr}.tsv",
+        header=None,
+        sep="\t",
+        dtype=type_dict
+    )
     # gdb_df = pd.read_csv(f"/mnt/ntc_data/wayne/Repositories/CRISPR/y_10w.tsv", header=None, sep="\t")
     # gdb 注释
     annotated_df = annotation_gdb(gdb_df, gene_pos_df)
     # 保存为tsv文件
     # annotated_df.to_csv(f"/mnt/ntc_data/wayne/Repositories/CRISPR/split_out/sorted/no_head/{chr}_gene.tsv", index=False, header=None, sep="\t")
     print(f"time cost:{time.time() - t}")
-    
+
 
 if __name__ == "__main__":
     main()
-    
