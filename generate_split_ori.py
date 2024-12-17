aaa55e734e03cdbf2ffc4acca4d82770ff8f4806
zzq89757 <1604653835@qq.com>
2024-12-17 06:09:20 +0000

    gtf extract repaired

--- generate_split_ori.py
+++ generate_split_ori.py
@@ -1,6 +1,8 @@
 from multiprocessing import Pool, RLock
 from os import system
 
+import pandas as pd
+
 def async_in_iterable_structure(fun, iterable_structure, cpus):
     def init(l):
         global lock
@@ -15,7 +17,9 @@ def async_in_iterable_structure(fun, iterable_structure, cpus):
     p.join()
 
 def run_cmd(i):
-    system(f"awk '$4==\"{i}\"' split_out/sorted/'spCas9_Homo(WGS)_gRNA-Original.tsv' > ori_split/ori_{i}.tsv")
+    # system(f"awk '$4==\"{i}\"' split_out/sorted/'spCas9_Homo(WGS)_gRNA-Original.tsv' > ori_split/ori_{i}.tsv")
+    print(f"awk '$1==\"{i}\"' GCF_000001405.40/*gtf > split_gtf/raw_gtf_split/{i}.gtf")
+    system(f"awk '$1==\"{i}\"' GCF_000001405.40/*gtf > split_gtf/raw_gtf_split/{i}.gtf")
 
 
 # for i in range(1,23):
@@ -26,8 +30,16 @@ def run_cmd(i):
 
 
 def main() -> None:
+    nc2chr_file = "nc2chr.tsv"
+    gtf_file = "GCF_000001405.40/GCF_000001405.40_GRCh38.p14_genomic.gtf"
+    # gtf_file = "ZNF568.gtf"
+    # num_count = Counter()
+    # info_dict = defaultdict()
+    # 读取nc2chr_file 生成 NC -> chr 的映射字典
+    nc_df = pd.read_csv(nc2chr_file, sep="\t", header=None)
+    nc_li = nc_df[0].tolist()
     chr_li = ["chr" + str(x) for x in (list(range(1, 23)) + ["X", "Y"])]
-    async_in_iterable_structure(run_cmd,chr_li,12)
+    async_in_iterable_structure(run_cmd,nc_li,12)
 
 if __name__ == "__main__":
     main()
--- process_gtf_app_bak.py
+++ process_gtf_app_bak.py
@@ -51,17 +51,11 @@ def append_gene_id_col(gtf_df: pd.DataFrame) -> None:
     """为gtf添加gene id 和 tran id 列"""
     # gtf_df[9] = gtf_df[8].str.split('"').str[1]
     # gtf_df[9] = gtf_df[8].apply(extract_gene_symbol)
-    # gtf_df[9] = gtf_df[8].str.extract(r'gene "([^"]+)"')
+    gtf_df[9] = gtf_df[8].str.extract(r'gene "([^"]+)"')
     # print(gtf_df[9])
-    # gtf_df[10] = gtf_df[8].str.split('"').str[3]
+    gtf_df[10] = gtf_df[8].str.split('"').str[3]
     # gtf_df[10] = gtf_df[8].str.extract(r'transcript_id "([^"]+)"')
     # print(gtf_df[10])
-    gtf_df[[10 ,9]] = gtf_df[8].str.extract(
-        # r'gene "([^"]+)"([^"]+)'
-        r'transcript_id "([^"]*)".*gene "([^"]*)";'
-    )
-    gtf_df[9] = gtf_df[9].fillna('')
-    gtf_df[10] = gtf_df[10].fillna('')
 
 
 def obtain_gene_id_li(gtf_df: pd.DataFrame) -> defaultdict:
@@ -72,6 +66,7 @@ def obtain_gene_id_li(gtf_df: pd.DataFrame) -> defaultdict:
     contains_bool_li3 = gene_df[8].str.contains(r"gene_biotype \"lncRNA\";")
     contains_bool_li = [x or y or z for x, y, z in zip(contains_bool_li1, contains_bool_li2, contains_bool_li3)]
     gene_df = gene_df[contains_bool_li]
+
     gene_id_li = gene_df[9].to_list()
     # 创建拆分的gtf的路径
     # for chr_name, sub_df in gene_df.groupby(0):
@@ -79,7 +74,7 @@ def obtain_gene_id_li(gtf_df: pd.DataFrame) -> defaultdict:
         # for gene in sub_df[9]:
         #     Path(f"split_gtf/{chr_name}/{gene}").mkdir(exist_ok=1,parents=1)
         # gene_df[[9,3,4,6]].to_csv(f"split_gtf/{chr_name}/{chr_name}.tsv",header=None,index=False,sep="\t")
-    return gene_id_li
+    return [gene_id_li, sum(contains_bool_li1), sum(contains_bool_li2)]
 
 
 def main() -> None:
@@ -87,9 +82,8 @@ def main() -> None:
     nc2chr_file = "nc2chr.tsv"
     gtf_file = "GCF_000001405.40/GCF_000001405.40_GRCh38.p14_genomic.gtf"
     gtf_file = "ZNF568.gtf"
-    gtf_file = f"split_gtf/raw_gtf_split/{nc_no}.gtf"
-    # num_count = Counter()
-    # info_dict = defaultdict()
+    num_count = Counter()
+    info_dict = defaultdict()
     # 读取nc2chr_file 生成 NC -> chr 的映射字典
     # nc_df = pd.read_csv(nc2chr_file, sep="\t", header=None)
     # nc2chr = dict(zip(nc_df[0], nc_df[1]))
@@ -102,7 +96,9 @@ def main() -> None:
     # 添加gene id 和tran id列
     append_gene_id_col(gtf_df)
     # 找到coding和uncoding的gene id 列表
-    gene_id_li = obtain_gene_id_li(gtf_df)
+    gene_id_li, num_count["coding_num"], num_count["noncoding_num"] = (
+        obtain_gene_id_li(gtf_df)
+    )
     # 按照gene id group by 只保留gene id 列表中且包含NM和NR的转录本（统计数目）
     # gb = gtf_df.groupby(0,sort=False)
     # print(sub_df)
@@ -110,13 +106,10 @@ def main() -> None:
     def cut_gtf(i):
         sub_df = gtf_df[gtf_df[0]==i]
         chr_name = i
-        Path(f"split_gtf/extract/{chr_name}").mkdir(exist_ok=1,parents=1)
         # sub_df = gb.get_group(chr_name)
     # for chr_name, sub_df in gtf_df.groupby(0,sort=False):
-        all_gene_li=[]
+        a=[]
         for gene_name, sub_gene_df in sub_df.groupby(9,sort=False):
-            tran_exon_li = pd.DataFrame([])
-            tran_cds_li = pd.DataFrame([])
             # print(sub_gene_df)
             append_flag = 1
             # 跳过coding和nc之外的基因
@@ -125,6 +118,7 @@ def main() -> None:
             # 跳过没有转录本的基因
             if len(sub_gene_df) == 1:
                 print(f"{gene_name} has no transcript found !!!")
+                num_count["no_tran_num"] += 1
                 continue
             # 记录基因信息
             for tran_id, sub_tran_df in sub_gene_df.groupby(10,sort=False):
@@ -134,7 +128,7 @@ def main() -> None:
                     continue
                 # 统计转录本类型数目
                 tran_prefix = tran_id.split("_")[0]
-                # num_count[tran_prefix] += 1
+                num_count[tran_prefix] += 1
                 # 选取含NM和NR转录本的gene 创建路径
                 if tran_prefix.startswith("N"):
                     # print(tran_id)
@@ -148,24 +142,17 @@ def main() -> None:
                             print(sub_gene_df)
                         gene_item[11] = "protein_coding" if gene_type_raw.find("RNA") == -1 else "non_coding" # type
                         # gene_item = gene_item[[9, 10, 3, 4, 6, 11]]
-                        all_gene_li.append(gene_item)
+                        a.append(gene_item)
                         append_flag = 0
                     
                     # 保存exon 和 cds 信息
-                    exon_df = sub_tran_df[sub_tran_df[2]=="exon"][[10, 3, 4, 6]]
-                    # print(exon_df)
-                    cds_df = sub_tran_df[sub_tran_df[2]=="CDS"][[10, 3, 4, 6]]
-                    tran_cds_li = pd.concat([tran_cds_li, cds_df])
-                    tran_exon_li = pd.concat([tran_exon_li, exon_df])
-            if len(tran_cds_li): # 若基因有CDS
-                pd.DataFrame(tran_cds_li).to_csv(f"split_gtf/extract/{chr_name}/{gene_name}_cds.tsv",header=None,index=False,sep="\t")
-                # print(tran_cds_li)
-                # print(sub_gene_df)
-                # print(len(tran_cds_li))
-            if len(tran_exon_li): # 若基因有exon
-                pd.DataFrame(tran_exon_li).to_csv(f"split_gtf/extract/{chr_name}/{gene_name}_exon.tsv",header=None,index=False,sep="\t")
+                    exon_df = sub_tran_df[sub_tran_df[2]=="exon"]
+                    cds_df = sub_tran_df[sub_tran_df[2]=="CDS"]
+                    Path(f"split_gtf/{chr_name}/{gene_name}").mkdir(exist_ok=1,parents=1)
+                    exon_df[[3,4,6]].to_csv(f"split_gtf/{chr_name}/{gene_name}/{tran_id}.exon",header=None,index=False,sep="\t")
+                    cds_df[[3,4,6]].to_csv(f"split_gtf/{chr_name}/{gene_name}/{tran_id}.cds",header=None,index=False,sep="\t")
         # 生成只含NM NR 的gene 表
-        pd.DataFrame(all_gene_li).to_csv(f"split_gtf/extract/{chr_name}/Gene_list.tsv",header=None,index=False,sep="\t")
+        pd.DataFrame(a).to_csv(f"split_gtf/{chr_name}/Gene_list.tsv",header=None,index=False,sep="\t")
     
     # async_in_iterable_structure(cut_gtf,["NC_000001.11","NC_000002.12"],2)
     # for i in ["NC_000001.11","NC_000002.12"]:
