53335fb8182faa65fb348c5d57ef0e029c4fc621
zzq89757 <1604653835@qq.com>
2025-01-02 03:22:16 +0000

    gdb2ot time cost start

---
+++ score/CFD_Scoring/test/time_cost.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# 将gdb的y染色体部分 分别扫各个染色体的ot库
+def gdb2ot(gdb_df: pd.DataFrame, ot_df: pd.DataFrame) -> pd.DataFrame:
+    ...
+    
+    
+def run_map(nc_no: str) -> None:
+    gdb_path = f"/mnt/ntc_data/wayne/Repositories/CRISPR/ag_mark/{nc_no}.tsv"
+    
+    ot_path = "/mnt/ntc_data/wayne/Repositories/CRISPR/sites_found/flashfry/db/"
+    
+    
+    
+def main() -> None:
+    run_map()
