13100576c99b86f1dff12572c7986e15db7da2a7
zzq89757 <1604653835@qq.com>
2025-02-21 01:34:31 +0000

    dual start

--- dual/dual.py
+++ dual/dual.py
@@ -5,3 +5,15 @@ from os import system
 path.append("/mnt/ntc_data/wayne/Repositories/CRISPR/")
 from utils.read_tsv import tsv2df
 from generate_split_ori import async_in_iterable_structure
+
+
+def dual() -> pd.DataFrame:
+    ...
+
+
+def main() -> None:
+    dual()
+
+
+if __name__ == "__main__":
+    main()
