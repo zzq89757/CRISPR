0cdcac929e6fbf990e7f8a2a38ce0070404384e1
zzq89757 <1604653835@qq.com>
2025-02-25 09:27:20 +0000

    filter

--- dual/dual.py
+++ dual/dual.py
@@ -3,13 +3,13 @@ import pandas as pd
 from sys import path
 from os import system
 path.append("/mnt/ntc_data/wayne/Repositories/CRISPR/")
-from utils.read_tsv import tsv2df
 from generate_split_ori import async_in_iterable_structure
-
+from utils.read_tsv import tsv2df
+pd.options.mode.copy_on_write = True
 
 def extract_pair_grna_info(pair_list: list) -> pd.DataFrame:
     # pair list 为二维数组 每个元素为成对df
-    
+
     # res list
     res_li = []
     for pair in pair_list:
@@ -19,10 +19,12 @@ def extract_pair_grna_info(pair_list: list) -> pd.DataFrame:
         grna1_offset = grna1[5].astype(str) + "0.5"
         grna2_offset = grna2[5].astype(str) + "0.5"
         # 计算距离
-        distance = int(abs((grna1[8] + grna1_offset.astype(float)).values[0] - (grna2[8] + grna2_offset.astype(float)).values[0]))
+        distance = int(abs((grna1[8] + grna1_offset.astype(float)).values[0] -
+                       (grna2[8] + grna2_offset.astype(float)).values[0]))
         # 分别提取每条grna的id、序列、pam、flank、location、tran cov、strand orientation、score、snp
         info_idx = [0, 2, 3, 21, 23, 4, 6, 7, 17, 5, 19, 22, 25]
-        merge_df = pd.concat([grna1[info_idx].iloc[0], grna2[info_idx].iloc[0]])
+        merge_df = pd.concat(
+            [grna1[info_idx].iloc[0], grna2[info_idx].iloc[0]])
         merge_df[26] = distance
         merge_df = merge_df.reset_index(drop=True)
         merge_df = merge_df[[26] + list(range(26))]
@@ -31,8 +33,6 @@ def extract_pair_grna_info(pair_list: list) -> pd.DataFrame:
         # print(merge_df)
         res_li.append(merge_df)
 
-        
-
 
 def is_pair() -> None:
     ...
@@ -40,9 +40,10 @@ def is_pair() -> None:
 
 def dual(raw_db: str) -> pd.DataFrame:
     # 读取filter20 数据库
-    df = tsv2df(raw_db,[])
-    # 按照基因分组 
-    for gene, sub_df in df.groupby(9):
+    df = tsv2df(raw_db, [])
+    # 按照基因分组
+    for gene, sub_df in df.groupby(9, sort=False):
+        print(gene,end="\t")
         grna_num = len(sub_df)
         # 若只有一条 无法成对 跳过
         if grna_num == 1:
@@ -57,17 +58,43 @@ def dual(raw_db: str) -> pd.DataFrame:
             continue
         # 两条以上的情况 暂时有放回
         sub_df = sub_df.reset_index(drop=True)
-        print(sub_df)
-        for i in range(grna_num):
-            grna1 =  grna_num.iloc[i]
-            # 方向不能与grna1方向相同
-            grna1_ori = grna1[5].values[0]
+        # 先按照方向分组
+        df_pos = sub_df[sub_df[5] == '+']
+        df_neg = sub_df[sub_df[5] == '-']
+        # 若过滤前组合数小于20 输出原始排列结果
+        if len(df_pos) * len(df_neg) <= 20:
+            ...
+            continue
+        # print(sub_df)
+        all_pair_num = 0
+        for _, grna1 in df_pos.iterrows():
+            # 两个分数差值都在20以内
+            cfd_score = grna1[19]
+            cfd_low = cfd_score - 20
+            cfd_high = cfd_score + 20
+            rs2_score = grna1[22]
+            rs2_low = rs2_score - 20
+            rs2_high = rs2_score + 20
+            candidate_df = df_neg[(df_neg[19] <= cfd_high) & (df_neg[19] >= cfd_low) & (
+                df_neg[22] <= rs2_high) & (df_neg[22] >= rs2_low)]
             # 间隔在50-1wbp
-            
-                
-            
-    
-
+            grna_start = grna1[6]
+            grna_end = grna1[7]
+            grna_middle = (grna_end + grna_start) / 2
+            middle_start_left = grna_middle - 10000
+            middle_end_left = grna_middle - 50
+            middle_start_right = grna_middle + 50
+            middle_end_right = grna_middle + 10000
+            candidate_df['grna_middle'] = (
+                candidate_df[6] + candidate_df[7]) / 2
+            candidate_df = candidate_df[
+                (middle_start_left < candidate_df['grna_middle']) & (candidate_df['grna_middle'] < middle_end_left) |
+                (middle_start_right < candidate_df['grna_middle']) & (
+                    candidate_df['grna_middle'] < middle_end_right)
+            ]
+            # print(len(candidate_df))
+            all_pair_num += len(candidate_df)
+        print(all_pair_num)
 
 def run_dual(nc_no: str) -> None:
     raw_db = f"/mnt/ntc_data/wayne/Repositories/CRISPR/filter_20/{nc_no}.tsv"
@@ -79,6 +106,7 @@ def run_dual(nc_no: str) -> None:
     return
     # 保存为tsv
     new_df.to_csv(output, sep="\t", header=None, index=None)
+
 
 def main() -> None:
     nc2chr_file = "/mnt/ntc_data/wayne/Repositories/CRISPR/nc2chr.tsv"
