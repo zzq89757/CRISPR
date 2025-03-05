616a42874c1a5acb7aded0528c64095897359db5
zzq89757 <1604653835@qq.com>
2025-03-05 05:34:39 +0000

    remove unused import

--- dual_20/dual.py
+++ dual_20/dual.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import time
 import pandas as pd
 from sys import path
-from os import system
 path.append("/mnt/ntc_data/wayne/Repositories/CRISPR/")
 from utils.read_tsv import tsv2df
 from generate_split_ori import async_in_iterable_structure
