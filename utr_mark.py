ada67ee42838c746cf80479fefa584638a3358d3
zzq89757 <1604653835@qq.com>
2025-04-04 01:36:02 +0000

    utr finished

--- utr_mark.py
+++ utr_mark.py
@@ -19,16 +19,31 @@ def utr_region_obtain(exon_file_path: str, cds_file_path: str, ori_dict: dict) -
         # print(sub_exon_df)
         # print(sub_cds_df)
         # print(gene_ori)
-        # 根据转录本分组 根据方向分别寻找UTR区域
+        # 根据转录本分组 
         # for (exon_tran, sub_exon_tran_df),(cds_tran, sub_cds_tran_df) in zip(sub_exon_df.groupby(1,sort=False), sub_cds_df.groupby(1,sort=False)):
         for exon_tran, sub_exon_tran_df in sub_exon_df.groupby(1,sort=False):
-            # 若exon对应转录本无cds（NR） 跳过
             sub_cds_tran_df = sub_cds_df[sub_cds_df[1]==exon_tran]
+            # 若exon对应转录本无cds（NR） 跳过
             if sub_cds_tran_df.empty:continue
-            print(exon_tran)
+            # print(exon_tran)
             print(sub_exon_tran_df)
             print(sub_cds_tran_df)
-        # 
+            # 根据方向分别寻找UTR区域
+            cds_start_idx = 0 if gene_ori == "+" else -1
+            cds_end_idx = -1 - cds_start_idx
+            cds_start = sub_cds_tran_df[2].to_numpy()[cds_start_idx]
+            cds_end = sub_cds_tran_df[3].to_numpy()[cds_end_idx]
+            exon_start = sub_exon_tran_df[2].to_numpy()[cds_start_idx]
+            exon_end = sub_exon_tran_df[3].to_numpy()[cds_end_idx]
+            utr5_start = exon_start
+            utr5_end = cds_start - 1
+            utr3_start = cds_end + 1
+            utr3_end = exon_end
+            # 将utr region 存入字典
+            
+            
+            # 
+            # utr_start = 
         
 
 
