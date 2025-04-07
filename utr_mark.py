ef66945370372543b7bf3e640dd7de770329d13a
zzq89757 <1604653835@qq.com>
2025-04-07 07:50:27 +0000

    merge regions start

--- utr_mark.py
+++ utr_mark.py
@@ -5,12 +5,19 @@ from cds_mark import gene_ori_dict
 from utils.read_tsv import tsv2df
 
 
+def merge_regions(region_li: list) -> list:
+    ...
+
+
 # 根据exon（含cds和UTR）和cds 计算5'UTR和3'UTR位置
 def utr_region_obtain(exon_file_path: str, cds_file_path: str, ori_dict: dict) -> defaultdict:
     # 读取exon和cds region 文件
     exon_df = tsv2df(exon_file_path,[])
     cds_df = tsv2df(cds_file_path,[])
     utr_region_dict = defaultdict(lambda :defaultdict(list))
+    cds_region_dict = defaultdict(list)
+    
+    
     # 按照基因分组
     for gene, sub_exon_df in exon_df.groupby(0,sort=False):
         # 若exon无cds（NR） 跳过
@@ -26,14 +33,19 @@ def utr_region_obtain(exon_file_path: str, cds_file_path: str, ori_dict: dict) -
             sub_cds_tran_df = sub_cds_df[sub_cds_df[1]==exon_tran]
             # 若exon对应转录本无cds（NR） 跳过
             if sub_cds_tran_df.empty:continue
+            # 记录所有cds区域
+            cds_start_li = sub_cds_tran_df[2].to_numpy()
+            cds_end_li = sub_cds_tran_df[3].to_numpy()
+            [cds_region_dict[gene].append((int(cds_start_li[i]), int(cds_end_li[i]))) for i in range(len(cds_end_li))]
+            
             # print(exon_tran)
             # print(sub_exon_tran_df)
             # print(sub_cds_tran_df)
             # 根据方向分别寻找UTR起始终止
             cds_start_idx = 0 if gene_ori == "+" else -1
             cds_end_idx = -1 - cds_start_idx
-            cds_start = sub_cds_tran_df[2].to_numpy()[cds_start_idx]
-            cds_end = sub_cds_tran_df[3].to_numpy()[cds_end_idx]
+            cds_start = cds_start_li[cds_start_idx]
+            cds_end = cds_end_li[cds_end_idx]
             exon_start = sub_exon_tran_df[2].to_numpy()[cds_start_idx]
             exon_end = sub_exon_tran_df[3].to_numpy()[cds_end_idx]
             utr5_start = int(exon_start)
@@ -44,10 +56,12 @@ def utr_region_obtain(exon_file_path: str, cds_file_path: str, ori_dict: dict) -
             utr_region_dict[gene]['UTR3'].append((utr3_start, utr3_end))
             utr_region_dict[gene]['UTR5'].append((utr5_start, utr5_end))
             
-    # 拆分为 0->真utr : utr 并集 - cds并集 -1-> 5UTR 1-> 3UTR
+    # 拆分为 0->真utr : utr 并集 - cds并集 -1-> 5UTR 1-> 3UTR 0,(-1/1)
+    
+    # 分别对UTR5 UTR3 CDS 求区域并集 
+    print(cds_region_dict)        
     print(utr_region_dict)        
     
-        
 
 
 
